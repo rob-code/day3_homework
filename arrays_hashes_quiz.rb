0bd713ab3d842f46ddc157ef4fbfafe83b5bc115
Robert Brice <rob.a.brice@gmail.comC>
2017-02-01 17:19:11 +0000

    Section 2

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -69,17 +69,45 @@ puts lines
 
 ### B. Given the following data structure:
 
+puts "------------------------------------"
+puts "Section B"
 my_hash = {"0" => "Zero", 1 => "One", :two => "Two", "two" => 2}
+
+puts my_hash
+
 
 # 1. How would you return the string `"One"`?
 
+puts ""
+puts "Q1"
+puts my_hash[1]
+
+
 # 2. How would you return the string `"Two"`?
+
+puts ""
+puts "Q2"
+puts my_hash[:two]
 
 # 3. How would you return the number `2`?
 
+puts ""
+puts "Q3"
+puts my_hash["two"]
+
 # 4. How would you add `{3 => "Three"}` to the hash?
 
+puts ""
+puts "Q4"
+my_hash[3] = "Three"
+puts my_hash
+
 # 5. How would you add `{:four => 4}` to the hash?
+
+puts ""
+puts "Q5"
+my_hash[:four] = 4
+puts my_hash
 
 ### C. Given the following data structure:
 
